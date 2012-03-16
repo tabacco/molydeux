499e0ff31b285b4563f9d943d1bd04225da1a423
Doug Tabacco <tabacco@dougtabacco.com>
2012-03-15 22:48:47 -0700

    Move the page header/footer to includes so we can use them on subpages

--- index.php
+++ index.php
@@ -1,29 +1,4 @@
-<!DOCTYPE html>
-<html>
-<head>
-
-  <title>What Would Molydeux? Molyjam 2012</title>
-
-  <link href="css/bootstrap.min.css" media="screen" rel="stylesheet" type="text/css" />
-  <link href="css/bootstrap-responsive.min.css" media="screen" rel="stylesheet" type="text/css" />
-  <link href="css/style.css" media="screen" rel="stylesheet" type="text/css" />
-
-  <!--This is a link to Google's Web Fonts for Open Sans -->  
-  <link href='http://fonts.googleapis.com/css?family=Open+Sans:700' rel='stylesheet' type='text/css'>  
-  
-  <meta name="viewport" content="width=device-width" />
-  <script type="text/javascript">
-    var _gaq = _gaq || [];
-    _gaq.push(['_setAccount', 'UA-6928584-3']);
-    _gaq.push(['_trackPageview']);
-    (function() {
-      var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-      ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-      var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-    })();
-  </script>
-</head>
-<body class="page" style="background-color: #000; margin: 0px;">
+<? require('templates/header.php'); ?>
   <div class="container">
     <div class="row">
       <div class="span10 offset1">
@@ -139,5 +114,4 @@
     </div> 
     
   </div>
-</body>
-</html>
+<? require('templates/footer.php'); ?>
---
+++ templates/header.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+  <title>What Would Molydeux? Molyjam 2012</title>
+
+  <link href="css/bootstrap.min.css" media="screen" rel="stylesheet" type="text/css" />
+  <link href="css/bootstrap-responsive.min.css" media="screen" rel="stylesheet" type="text/css" />
+  <link href="css/style.css" media="screen" rel="stylesheet" type="text/css" />
+
+  <!--This is a link to Google's Web Fonts for Open Sans -->  
+  <link href='http://fonts.googleapis.com/css?family=Open+Sans:700' rel='stylesheet' type='text/css'>  
+  
+  <meta name="viewport" content="width=device-width" />
+  <script type="text/javascript">
+    var _gaq = _gaq || [];
+    _gaq.push(['_setAccount', 'UA-6928584-3']);
+    _gaq.push(['_trackPageview']);
+    (function() {
+      var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+      ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+      var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+    })();
+  </script>
+</head>
+<body class="page" style="background-color: #000; margin: 0px;">
+
